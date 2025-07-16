49a2de59b4fe88696f607b9760b6b53380bcdf08
Tengku Hamza <66053661+Ph4ntomX@users.noreply.github.com>
2025-07-16 10:44:44 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,13 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import TodoList from './components/TodoList'
 import TodoForm from './components/TodoForm'
 
-function useLocalStorage(key, initialValue) {
-  // Get initial value from localStorage or use initialValue
-  const [storedValue, setStoredValue] = useState(() => {
-    try {
-      const item = window.localStorage.getItem(key);
-      return item ? JSON.parse(item) : initialValue;
-    } catch (error) {
-      console.error(error);
-      return initialValue;
-    }
-  });
-
-  // Update localStorage whenever the storedValue changes
-  useEffect(() => {
-    try {
-      window.localStorage.setItem(key, JSON.stringify(storedValue));
-    } catch (error) {
-      console.error(error);
-    }
-  }, [key, storedValue]);
-
-  return [storedValue, setStoredValue];
-}
-
-
 function App() {
-
-  const [tasks, setTasks] = useLocalStorage('tasks', [])
+  const [tasks, setTasks] = useState([])
 
   return (
     <>
-      <div
-      className="card rounded shadow-sm"
-      style={{ maxWidth: '500px', margin: '60px auto' }}
-    >
+      <div className="card rounded shadow-sm" style={{ maxWidth: '500px', margin: '60px auto' }}>
       <div className="card-body">
         <h3 className="card-title mb-3">My Todo List</h3>
 
